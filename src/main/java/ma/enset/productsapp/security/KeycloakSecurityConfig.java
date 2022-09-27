ddaeb117169ffe5bad4be07bfefa6144fa6cf618
syrjay <sy.jroumar@gmail.com>
2022-09-27 14:13:19 +0100

    problem with PagedModel

--- src/main/java/ma/enset/productsapp/security/KeycloakSecurityConfig.java
+++ src/main/java/ma/enset/productsapp/security/KeycloakSecurityConfig.java
@@ -1,19 +1,27 @@
 package ma.enset.productsapp.security;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+// import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-
-@Configuration
+// @Configuration
+@KeycloakConfiguration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
   @Override
@@ -22,22 +30,28 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
   }
 
   // gestion des user
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    // dire à spring boot que je ne gère pas les users et les roles, je le délègue
-    auth.authenticationProvider(keycloakAuthenticationProvider());
-  }
+  @Autowired
+   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+       KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+       keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+       auth.authenticationProvider(keycloakAuthenticationProvider);
+   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     super.configure(http);
     // pour accéser à /product il faut s'authentifier d'abord
-    http.authorizeRequests().antMatchers("/product/**", "/suppliers/**").authenticated();
+    http.authorizeRequests().antMatchers("/products/**", "/suppliers/**").authenticated();
   }
 
-  @Bean
+  /* @Bean
   public KeycloakSpringBootConfigResolver springBootConfigResolver() {
     return new KeycloakSpringBootConfigResolver();
+  } */
+
+  @Bean
+  public KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory keycloakClientRequestFactory) {
+    return new KeycloakRestTemplate(keycloakClientRequestFactory);
   }
   
 }
--- src/main/java/ma/enset/productsapp/web/ProductController.java
+++ src/main/java/ma/enset/productsapp/web/ProductController.java
@@ -1,13 +1,30 @@
 package ma.enset.productsapp.web;
 
 import ma.enset.productsapp.repositories.ProductRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Data;
 
 @Controller
 public class ProductController{
+
+    @Autowired
+    public KeycloakRestTemplate keycloakRestTemplate;
     @Autowired
     private ProductRepository productRepository;
 
@@ -21,8 +38,30 @@ public class ProductController{
         return "products";
     }
     @GetMapping("/suppliers")
-    public String suppliers(){
+    public String suppliers(Model model){
+        // exécute avec token
+        PagedModel<Supplier> pageSuppliers = keycloakRestTemplate.getForObject("http://localhost:8083/suppliers", PagedModel.class);
+        model.addAttribute("suppliers", pageSuppliers);
         return "suppliers";
     }
 
+    @GetMapping("/jwt")
+    @ResponseBody
+    public Map<String, String> map(HttpServletRequest request) {
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+        // c'est ici qu'est stocké les infos sur le user connecté
+        KeycloakSecurityContext keycloakSecurityContext = principal.getKeycloakSecurityContext();
+        Map<String, String> map = new HashMap<>();
+        map.put("access_token", keycloakSecurityContext.getTokenString());
+        return map;
+    }
+
+}
+
+@Data
+class Supplier {
+    private Long id;
+    private String name;
+    private String email;
 }
